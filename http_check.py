9066a61a93862969b252650539368db4c86ea28d
loydi <32053602+loydi@users.noreply.github.com>
2018-08-03 10:12:31 +0300

    http check

---
+++ http_check.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+import requests,argparse,urllib3
+
+urllib3.disable_warnings()
+def check(arg1):
+    r = requests.get(arg1)
+    
+    if r.status_code ==200:
+        print ('System is OK.. : OK')
+        exit (0)
+    else:
+        print ('Can not access site')
+        exit (2)
+
+#Argüman tanımlama
+parser = argparse.ArgumentParser()
+parser.add_argument("--url","-u",help="Site Url")
+veri = parser.parse_args()
+#print("site: {}".format(veri.site))
+
+
+if __name__ == '__main__':
+    check(veri.url)
